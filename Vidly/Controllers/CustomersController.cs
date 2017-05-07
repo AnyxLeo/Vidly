632c546231d942257d5caf416c25418c1c2e3622
Ana Maria Atayde Peña <damsa.aatayde@damsa.com.mx>
2017-05-07 13:00:28 -0500

    Primer commit

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer {Id = 1, Name = "Anita la huerfanita"},
+                new Customer {Id = 2, Name = "X-men"}
+            };
+
+            var viewModel = new CustomerViewModel
+            {
+                Customers = customers
+            };
+            return View(viewModel);
+        }
+
+        //GET: Customers/Details
+        public ActionResult Details(int id)
+        {
+            var customers = new List<Customer>
+            {
+                new Customer {Id = 1, Name = "Anita la huerfanita"},
+                new Customer {Id = 2, Name = "X-men"}
+            };
+            var customer = customers.Find(c => c.Id == id);
+
+            if (customer!=null)
+            {
+                return View(customer);
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+            
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = new List<Movie>
+            {
+                new Movie { Name = "Sherk"},
+                new Movie { Name = "Wall-e"},
+                new Movie { Name = "Valiente"}
+            };
+            var viewModel = new MovieViewModel
+            {
+                Movies = movies
+            };
+
+            return View(viewModel);
+        }
+    }
+}
